2da255da84ecd8cac19ab58fae5e741618e169d6
Kaladziuk, Stanislau <stanislau.kaladziuk@ihg.com>
2018-09-19 14:02:24 +0300

    Up and running but with paths problems

--- src/main/java/by/kolodyuk/osgi/springboot/SpringBootBundleActivator.java
+++ src/main/java/by/kolodyuk/osgi/springboot/SpringBootBundleActivator.java
@@ -1,15 +1,25 @@
 package by.kolodyuk.osgi.springboot;
 
+import by.kolodyuk.osgi.springboot.controller.SimpleController;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+// @Import(EmbeddedServletContainerAutoConfiguration.EmbeddedTomcat.class)
+@Import(SimpleController.class)
+@SpringBootApplication(exclude = {SpringDataWebAutoConfiguration.class, SecurityAutoConfiguration.class, TransactionAutoConfiguration.class})
 public class SpringBootBundleActivator implements BundleActivator {
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
         SpringApplication.run(SpringBootBundleActivator.class);
     }
 
---
+++ src/main/java/by/kolodyuk/osgi/springboot/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package by.kolodyuk.osgi.springboot.controller;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/echo")
+    public String echo() {
+        return "echo";
+    }
+
+}
